9cdf5f98050fef2406cb86d0bf07ed23a5648d57
Maria Eduarda <101368201+Maria22027@users.noreply.github.com>
2024-11-09 14:41:53 -0300

    Delete atv.firestone directory

--- atv.firestone/app/src/main/java/br/edu/fatecpg/appfirestore/models/Produto.kt
+++
@@ -1,9 +0,0 @@
-package br.edu.fatecpg.appfirestore.models
-
-import java.io.Serializable
-
-data class Produto(
-    val nome:String,
-    val categoria:String,
-    val preco:Double
-): Serializable
